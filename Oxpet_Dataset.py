300b68fe45db6cb7d5d10806b7bcab2e25d557b0
王可凡 <zcabkwa@ucl.ac.uk>
2021-12-24 14:12:37 +0000

    Update Oxpet_Dataset.py
    Now can control which target values are required.

--- Oxpet_Dataset.py
+++ Oxpet_Dataset.py
@@ -1,12 +1,21 @@
 import h5py
 from torch.utils.data import Dataset
+# from torch.utils.data import DataLoader
+# import numpy as np
+# import os
 
 class Oxpet_Dataset(Dataset):
-    def __init__(self, img_path, binary_path, bboxes_path, masks_path):
+    def __init__(self, img_path, binary_path, bboxes_path, masks_path, require_binary = True, require_bbox = True, require_masks = True):
         self.img_path = img_path
-        self.binary_path = binary_path
-        self.bboxes_path = bboxes_path
-        self.masks_path = masks_path
+        self.require_binary = require_binary
+        self.require_bbox = require_bbox
+        self.require_masks = require_masks
+        if self.require_binary:
+            self.binary_path = binary_path
+        if self.require_bbox:
+            self.bboxes_path = bboxes_path
+        if self.require_masks:
+            self.masks_path = masks_path
 
     def __len__(self):
         with h5py.File(self.binary_path,"r") as f:
@@ -22,17 +31,30 @@ class Oxpet_Dataset(Dataset):
         with h5py.File(self.img_path,"r") as f:
             key = list(f.keys())[0]
             img = f[key][idx]
-        with h5py.File(self.binary_path,"r") as f:
-            key = list(f.keys())[0]
-            binary = f[key][idx]
-        with h5py.File(self.bboxes_path,"r") as f:
-            key = list(f.keys())[0]
-            bboxes = f[key][idx]
-        with h5py.File(self.masks_path,"r") as f:
-            key = list(f.keys())[0]
-            masks = f[key][idx]
         
-        return (img, binary, bboxes, masks)
+        if self.require_binary:
+            with h5py.File(self.binary_path,"r") as f:
+                key = list(f.keys())[0]
+                binary = f[key][idx]
+        
+        if self.require_bbox:
+            with h5py.File(self.bboxes_path,"r") as f:
+                key = list(f.keys())[0]
+                bboxes = f[key][idx]
+        if self.require_masks:
+            with h5py.File(self.masks_path,"r") as f:
+                key = list(f.keys())[0]
+                masks = f[key][idx]
+        
+        result = [img]
+        if self.require_binary:
+            result.append(binary)
+        if self.require_bbox:
+            result.append(bboxes)
+        if self.require_masks:
+            result.append(masks)
+        return result
 
-# training_data = Oxpet_Dataset(os.path.join("datasets-oxpet", "train","images.h5"),os.path.join("datasets-oxpet", "train","binary.h5"),os.path.join("datasets-oxpet", "train","bboxes.h5"),os.path.join("datasets-oxpet", "train","masks.h5"))
+# training_data = Oxpet_Dataset(os.path.join("datasets-oxpet", "train","images.h5"),os.path.join("datasets-oxpet", "train","binary.h5"),os.path.join("datasets-oxpet", "train","bboxes.h5"),os.path.join("datasets-oxpet", "train","masks.h5"), require_binary=False, require_masks=False)
+# print(len(training_data.__getitem__(0)))
 # ox_dataloader = DataLoader(training_data, batch_size=32, shuffle= True,num_workers=4)
