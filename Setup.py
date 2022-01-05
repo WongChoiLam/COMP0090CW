f17ecf9f79ec8f3b4d0d227a01b74d65ae2dc06c
王可凡 <zcabkwa@ucl.ac.uk>
2022-01-05 00:56:56 +0000

    Add gzip compression in setup

--- Setup.py
+++ Setup.py
@@ -2,19 +2,19 @@ import h5py
 import os
 
 def rewrite_h5py(dataset_path):
-    os.mkdir(f'{dataset_path}-rewrited')
+    os.mkdir(f'{dataset_path}-rewritten')
     for directory in os.listdir(f'{dataset_path}'):
         if directory.endswith('md'):
             continue
-        os.mkdir(os.path.join(f'{dataset_path}-rewrited',directory))
+        os.mkdir(os.path.join(f'{dataset_path}-rewritten',directory))
         for file in os.listdir(os.path.join(dataset_path,directory)):
             all_data = None
             with h5py.File(os.path.join(dataset_path,directory,file),"r") as f:
                 key = list(f)[0]
                 all_data = f[key][()]
-            with h5py.File(os.path.join(f'{dataset_path}-rewrited',directory,file), "w") as f:
+            with h5py.File(os.path.join(f'{dataset_path}-rewritten',directory,file), "w") as f:
                 for i,data in enumerate(all_data):
-                    f[f'{i}'] = data
+                    f.create_dataset(f'{i}', data=data, compression='gzip')
 
 if __name__ == '__main__':
     import time
