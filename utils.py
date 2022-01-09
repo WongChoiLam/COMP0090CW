c746b3d4972612e498ea06080f01632a117a153f
王可凡 <zcabkwa@ucl.ac.uk>
2022-01-09 20:39:21 +0000

    Delete DiceLoss

--- utils.py
+++ utils.py
@@ -1,26 +1,4 @@
-import torch.nn as nn
 import torch
-
-class DiceLoss(nn.Module):
-
-    'DiceLoss is adapted from https://www.kaggle.com/bigironsphere/loss-function-library-keras-pytorch'
-    
-    def __init__(self, weight=None, size_average=True):
-        super(DiceLoss, self).__init__()
-
-    def forward(self, inputs, targets, smooth=1):
-        
-        #comment out if your model contains a sigmoid or equivalent activation layer
-        inputs = torch.sigmoid(inputs)       
-        
-        #flatten label and prediction tensors
-        inputs = inputs.view(-1)
-        targets = targets.view(-1)
-        
-        intersection = (inputs * targets).sum()                            
-        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
-        
-        return 1 - dice
 
 def Evaluation_mask(model, training_data, targets):
 
