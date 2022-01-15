4800cb8ce21b0908e4261fb2a46f03553d486824
ilusm603 <854905420@qq.com>
2022-01-15 17:01:43 +0000

    upload new stats

--- Ablation_deeplabv3_resnet50.py
+++ Ablation_deeplabv3_resnet50.py
@@ -161,7 +161,7 @@ if __name__ == '__main__':
     stat_file_name = 'Ablation_stats.csv'
 
     # Output the stats
-    with open(stat_file_name, 'w') as f:
+    with open(stat_file_name, 'w',newline='') as f:
 
         write = csv.writer(f)
         write.writerow(train_loss)
